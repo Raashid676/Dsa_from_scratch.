2a76fdbb1f28a4f306488f3e818337d910792675
Mohammmad_Raashid <abr082435@gmail.com>
2026-07-31 12:24:09 +0530

    0543-DIAMETER-OF-BINARY-TREE Stats: Time: 0 ms (100%), Space: 23.5 MB (94.54%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    int ans = 0;
     int height(TreeNode* root){
 
         if(root == NULL){
@@ -20,19 +21,16 @@ public:
         int leftH = height(root->left);
         int rightH = height(root->right);
 
+        ans = max(ans, leftH + rightH);
+
         return max(leftH, rightH) + 1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-
-        int leftDia = diameterOfBinaryTree(root->left);
-        int rightDia = diameterOfBinaryTree(root->right);
-        int currDia = height(root->left) + height(root->right);
+        height(root);
 
 
-        return max(currDia, max(leftDia, rightDia));
+
+        return ans;
         
     }
 };
