a0786a7baa0b55b32cea55c15bc4b5601d983e64
Mohammmad_Raashid <abr082435@gmail.com>
2026-08-14 12:25:05 +0530

    0173-BINARY-SEARCH-TREE-ITERATOR Stats: Time: 4 ms (53.59%), Space: 32 MB (35.13%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,32 @@
+
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    void storeLeftNodes(TreeNode* root){
+        while(root != NULL){
+
+            s.push(root);
+            root = root->left;
+        }
+
+    }
+    BSTIterator(TreeNode* root) {
+
+        storeLeftNodes(root);
+        
+    }
+    
+    int next() {
+        TreeNode* ans = s.top();
+        s.pop();
+        storeLeftNodes(ans->right);
+        return ans->val;
+        
+    }
+    
+    bool hasNext() {
+        return s.size() > 0;
+         
+    }
+};
+
