3d984cf5336e8bf21c43d9382bc5fcd98738526a
Mohammmad_Raashid <raashid2341@gmail.com>
2026-09-18 12:06:59 +0530

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 96 ms (5.11%), Space: 46.2 MB (64.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,28 +1,33 @@
 class Solution {
+    private int find(int[] a) {
+        int maxc = -1;
+        for (int i = 0; i < 256; i++)
+            maxc = Math.max(maxc, a[i]);
+        return maxc;
+    }
+
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        int low = 0;
-        int res = 0;
-        int maxFreq = 0;
-        Map<Character, Integer> freq = new HashMap<>();
+        int[] f = new int[256];
+        int low = 0, high = 0, res = Integer.MIN_VALUE;
 
-        for(int right = 0; right < n; right++){
-            char c = s.charAt(right);
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, freq.get(c));
+        for (high = 0; high < n; high++) {
+            f[s.charAt(high)]++;
+            int maxcnt = find(f);
+            int len = high - low + 1;
+            int diff = len - maxcnt;
 
-            // shrink window if (window size - maxFreq) > k
-            while((right - low + 1) - maxFreq > k){
-                char left = s.charAt(low);
-                freq.put(left, freq.get(left) - 1);
-                if(freq.get(left) == 0)
-                    freq.remove(left);
+            while (diff > k) {
+                f[s.charAt(low)]--;
                 low++;
+                maxcnt = find(f);
+                len = high - low + 1;
+                diff = len - maxcnt;
             }
 
-            res = Math.max(res, right - low + 1);
+            len = high - low + 1;
+            res = Math.max(res, len);
         }
-
         return res;
     }
 }
